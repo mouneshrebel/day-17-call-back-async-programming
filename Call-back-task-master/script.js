00bed27de81f698bd41ef232ff70bee77014101f
mounesh <122527299+mouneshrebel@users.noreply.github.com>
2024-02-18 22:02:46 +0530

    Add files via upload

---
+++ Call-back-task-master/script.js
@@ -0,0 +1,53 @@
+
+
+
+ele=document.getElementById("demo")
+
+
+
+
+
+
+
+    setTimeout(()=>{
+        ele.innerText="10";
+        setTimeout(()=>{
+            ele.innerText="9";
+            setTimeout(()=>{
+                ele.innerText="8";
+                setTimeout(()=>{
+                    ele.innerText="7";
+                    setTimeout(()=>{
+                        ele.innerText="6";
+                        setTimeout(()=>{
+                            ele.inerText="5";
+                            setTimeout(()=>{
+                                ele.innerText="4";
+                                setTimeout(()=>{
+                                    ele.innerText="3";
+                                    setTimeout(()=>{
+                                        ele.innerText="2";
+                                        setTimeout(()=>{
+                                            ele.innerText="1";
+                                            setTimeout(()=>{
+                                                ele.innerText="Happy Independence Day"
+                                                setTimeout(()=>{
+                                                    ele.innerText="Jai-Hind" ;
+                                                    setTimeout(()=>{ele.innerText=""})  
+                                                                                                     
+                                                },1000)
+                                            },1000)
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+    
+
+
+
